0a12011a4653fc03e4ba62b05ce75e0fc966456e
Lenochkaa <flylilianagud@gmail.com>
2022-02-17 21:44:10 +0200

    Update main.js

--- main.js
+++ main.js
@@ -59,35 +59,35 @@
 
 // let title = "Мой лучший враг"
 // function favorite_book(title){
-//     alert(One of my favorite book is ${title})
+//     alert(`One of my favorite book is ${title}`)
 // }
 // favorite_book(title)
 
 // let size = prompt("Введите размер футболки (S,M,L,XL).")
 // let text =prompt("Введите текст футболки")
 // function make_shirt(size, text){
-//     alert(Вы выбрали футболку с размером ${size} и текстом "${text}".)
+//     alert(`Вы выбрали футболку с размером ${size} и текстом "${text}".`)
 // }
 // make_shirt(size, text)
 
 
-// function make_shirt(a, b){
-//     let size = "L"
-//     let text = "I love JS"
-//     alert(Вы выбрали футболку с размером ${size} и текстом "${text}".)
-// }
-// let a = prompt("Введите размер футболки (S,M,L,XL).")
-// let b =prompt("Введите текст футболки")
-// make_shirt(a, b)
-// alert(Вы выбрали футболку с размером ${a} и текстом "${b}".)
+function make_shirt(a, b){
+    let size = "L"
+    let text = "I love JS"
+    alert(`Вы выбрали футболку с размером ${size} и текстом "${text}".`)
+}
+let a = prompt("Введите размер футболки (S,M,L,XL).")
+let b =prompt("Введите текст футболки")
+make_shirt(a, b)
+alert(`Вы выбрали футболку с размером ${a} и текстом "${b}".`)
 
 // let c = prompt("Введите название города")
 // function describe_city(a){
 //     let count = "Украина"
-//     alert(${c} это ${count})
+//     alert(`${c} это ${count}`)
 // }
 // let a = prompt("Введите название страны")
-// alert(${c} это ${a})
+// alert(`${c} это ${a}`)
 // describe_city(a)
 // describe_city(a)
 // describe_city(a)
